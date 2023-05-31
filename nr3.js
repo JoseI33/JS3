87ddf5174ff4924792b7a346e9ca7c78c1692235
Jose <jose.imhoff.20@gmail.com>
2023-05-30 23:42:19 -0300

    tp3

---
+++ nr3.js
@@ -0,0 +1,35 @@
+/*1 - Crear un array llamado meses 
+que almacene el nombre de los doce meses del año. 
+Mostrar por pantalla en forma de lista los doce nombres del arreglo.
+
+let meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre','noviembre', 'diciembre'];
+
+for (let i = 0; i < meses.length; i++) {
+  console.log(meses[i]);
+  
+}*/
+
+/*2-  Crear un script que solicite al usuario mediante un prompt el nombre de ciudades 
+y almacenarlas en un arreglo, 
+cuando el usuario selecciona cancelar se debe mostrar el arreglo generado, 
+luego realizar las siguientes acciones:
+
+Mostrar la longitud del arreglo.
+Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+Añade en última posición la ciudad de París.
+Escribe por pantalla el elemento que ocupa la segunda posición.
+Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
+
+let ciudades = prompt("Coloque el nombre de ciudades.");
+let i = [];
+    
+while (ciudades !== "cancelar") {
+    i.push(ciudades);
+}
+
+console.log(i);*/
+
+
+
+
+    
